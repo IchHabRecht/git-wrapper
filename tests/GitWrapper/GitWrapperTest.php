7d834a9d687e1e22b34494f4af46574d08bf1f1d
Nicole Cordes <typo3@cordes.co>
2016-05-05 23:16:22 +0200

    [TASK] Add Unit Tests for GitWrapper class

---
+++ tests/GitWrapper/GitWrapperTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace IchHabRecht\GitWrapper\Test;
+
+use IchHabRecht\GitWrapper\GitRepository;
+use IchHabRecht\GitWrapper\GitWrapper;
+
+class GitWrapperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var GitWrapper
+     */
+    protected $gitWrapper;
+
+    protected function setUp()
+    {
+        $this->gitWrapper = new GitWrapper();
+    }
+
+    public function testExecuteVersion()
+    {
+        $this->assertStringStartsWith('git version ', $this->gitWrapper->execute('version'));
+    }
+
+    public function testGetRepositoryReturnsInstance()
+    {
+        $directory = tempnam('git-wrapper', 'foo');
+        $repository = $this->gitWrapper->getRepository($directory);
+
+        $this->assertInstanceOf(GitRepository::class, $repository);
+    }
+}
